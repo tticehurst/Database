564b37c22b2d2644e1ae75e51f18aa9d9a28d3ea
O5X <tolly67p@gmail.com>
2020-07-01 19:38:29 +0100

    Tkinter calc should be done

--- Python/SimpleCalculatorTkinter.py
+++ Python/SimpleCalculatorTkinter.py
@@ -17,8 +17,61 @@ class Calculator:
         b1 = self.createButton(7)
         b2 = self.createButton(8)
         b3 = self.createButton(9)
-        b4 = self.createButton(u"\u232B",None)
+        b4 = self.createButton(🔙)
+        b5 = self.createButton(4)
+        b6 = self.createButton(5)
+        b7 = self.createButton(6)
+        b8 = self.createButton(➗)
+        b9 = self.createButton(1)
+        b10 = self.createButton(2)
+        b11 = self.createButton(3)
+        b12 = self.createButton(✖)
+        b13 = self.createButton(.)
+        b14 = self.createButton(0)
+        b15 = self.createButton(➕)
+        b16 = self.createButton(➖)
+        b17 = self.createButton('=',None,34)
 
+        # Buttons stored in list
+        buttons =
+    [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17]
+
+    # Initialize counter
+    count = 0
+    # Arrange buttons with grid manager
+    for row in range(1,5) :
+        for column in range(4) :
+            buttons[count].grid(row=row,column=column)
+            count += 1 # Arranges buttons starting at 1 and going up
+    # Arrange last button '=' at the bottom
+    buttons[17].grid(row=5,column=0,columnspan=4)
+
+    def createButton(self,val,write=True,width=7) :
+        # This function creates a button, and takes one compulsory argument,
+        # the value that should be on the button
+
+    return Button(self.master, text=val,command = lambda: self.click(val,write), width=width)
+
+    def click(self,text,write):
+        # This function handles what happens when you click a button.
+        # 'write' argument if True means the value 'val' should be written on screen.
+        # If None, nothing should not be written on screen.
+        if write == None:
+        # Only evaluate code when there is an equation to be evaluated
+        self.insert_screen(text)
+
+    def clear_screen(self):
+        # Sets equation to empty before deleting screen
+        self.equation = ''
+        self.screen.configure(state='normal')
+        self.screen.delete('1.0', END)
+
+    def insert_screen(self, value,newline=False):
+        self.screen.configure(state='normal')
+        self.screen.insert(END,value)
+        # Records every value inserted in screen
+        self.equation += str(value)
+        self.screen.configure(state='disabled')
 
 root = Tk()
 my_calc = Calculator(root)
