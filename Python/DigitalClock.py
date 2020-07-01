0ef17ea57a79e7d4a4dcfb9447317d9f29320533
O5X <tolly67p@gmail.com>
2020-07-01 20:54:48 +0100

    DigialClock is done!

---
+++ Python/DigitalClock.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from time import *
+
+root = Tk()
+root.title('Clock')
+
+def time():
+    string = strftime('%H:%M:%S')
+    lbl.config(text = string)
+    lbl.after(1, time)
+
+lbl = Label(root, font = ('calibri', 40, 'bold'),
+            background = 'black',
+            foreground = 'white')
+
+lbl.pack(anchor = 'center')
+time()
+mainloop()
