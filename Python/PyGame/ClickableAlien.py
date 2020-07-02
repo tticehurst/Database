90de2ce6b85e8bc41ab5e72f47ffbd7324f733e5
O5X <tolly67p@gmail.com>
2020-07-02 18:59:07 +0100

    Fixed clickable alien

--- Python/PyGame/ClickableAlien.py
+++ Python/PyGame/ClickableAlien.py
@@ -1,25 +1,19 @@
 import pgzrun
 
 alien = Actor('alien')
-alien.topright = 0,10
+alien.pos = 100,56
 
 WIDTH = 300
 # HEIGHT = 300
 HEIGHT = alien.height + 20
 
-def update():
-    alien.left +=2 # Sets speed
-    if alien.left > WIDTH:
-        alien.right = 0
+def on_mouse_down():
+    print("Click!")
 
-def on_mouse_click(pos):
-    if alien.collidepoint(pos):
-        print("Eek!")
-    else:
-        print("You Missed!")
 
 def draw():
     screen.clear()
     alien.draw()
+
 
 pgzrun.go()
