4cf8f7daa05ba285fa88ea88cdd1e87fe9176436
O5X <tolly67p@gmail.com>
2020-07-04 13:41:50 +0100

    Completed SoundsAndImage.py

--- Python/PyGame/SoundsAndImages.py
+++ Python/PyGame/SoundsAndImages.py
@@ -1,6 +1,6 @@
 import pgzrun
 
-alien = Actor('alien')
+alien = Actor('alien_normal')
 alien.pos = 100,56
 
 WIDTH = 300
@@ -8,8 +8,18 @@ HEIGHT = alien.height + 20
 
 def on_mouse_down(pos):
     if alien.collidepoint(pos):
-        sounds.alien_hurt.play()
-        alien.image = 'alien_hurt'
+        set_alien_hurt()
+        clock.schedule_unique(set_alien_normal, 1.0)
+
+def set_alien_hurt():
+    alien.image = 'alien_hurt'
+    sounds.alien_hurt.play()
+    
+
+def set_alien_normal():
+    alien.image = 'alien_normal'
+
+
 
 def draw():
     screen.clear()
