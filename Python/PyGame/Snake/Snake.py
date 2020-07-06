3872f3474e2695bad00a0324f19e0e209d5af371
O5X <tolly67p@gmail.com>
2020-07-06 18:37:47 +0100

    Done collisions and drawing

--- Python/PyGame/Snake/Snake.py
+++ Python/PyGame/Snake/Snake.py
@@ -58,5 +58,41 @@ class Food():
 class Player():
     # Initialization
     def __init__(self, screen):
-        self.x= screen.width/2
-        self.y
+        self.x = screen.width/2
+        self.y = screen_height/2
+        self.width = 10
+        self.height = 10
+        self.velocity = 10
+        self.direction = 'stop'
+        self.body = []
+        self.head.color = green
+        self.body.color = brown
+    # Draws the snake on the given surface
+    def draw_player(self, surface):
+        self.seg = []
+        self.head = pg.Rect(self.x, self.y, self.width, self.height)
+        pg.draw.rect(surface, self.head_color, self.head)
+        if len(self.body) > 0:  # len = Length # If the length of the body is more than 0
+            for unit in self.body:
+                segment = pg.Rect(unit[0], unit[1], self.width, self.height)
+                pg.draw.rect(surface, self.body_color, segment) # Telling it where it moves (I think)
+                self.seg.append(segment)
+    # Increases length of the snake
+    def add_unit(self):
+        if len(self.body) != 0: # if lenghth is not equal to 0
+            index.len(self.body)-1
+            x = self.body[index][0]
+            y = self.body[index][1]
+            self.body.append([x,y])
+            
+        else:
+            self.body.append([1000,1000])
+    # Checks if there is a collision
+    def is_collision(self):
+        # Collision with itself
+        for segment in self.seg:
+            if self.head.colliderect(segment):
+                return True
+        # Collision with the boundries
+        if self.y < 0 or self.y > screen_height - self.height or self.x < 0 or self.x > screen_width - self.width:
+            return True
