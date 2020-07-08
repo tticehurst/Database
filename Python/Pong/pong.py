1b621c274e81a23a40ba2480a08450e01d069fb9
O5X <tolly67p@gmail.com>
2020-07-08 17:47:50 +0100

    Initial Commit

---
+++ Python/Pong/pong.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+
+# Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+# Open a new window
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Pong!")
