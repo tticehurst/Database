716f2996940827f35f6948d1b7607b4972d6f4a1
O5X <tolly67p@gmail.com>
2020-07-08 17:13:55 +0100

    Removed program since it could only log one line. Easier to do manually

--- Python/Logging/Logging2.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO, filename='moj.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M')
-
-name= 'bob'
-logging.info('%s - Resolved as invalid', name)
