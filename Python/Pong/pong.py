1b3ec94b8aaa15c037e58a44335261b4d1c759b9
O5X <tolly67p@gmail.com>
2020-07-08 17:56:15 +0100

    Created middle white line

--- Python/Pong/pong.py
+++ Python/Pong/pong.py
@@ -22,4 +22,18 @@ while carryOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
-            
+
+    # Game logic should go here
+
+
+    # Drawing code should go here
+    # First, clear the screen to black
+    screen.fill(BLACK)
+    # Draws the white line in the middle
+    pygame.draw.line(screen, WHITE, [349,0], [349, 500], 5) # 349 = Top half and bottom half
+    # Update the display with what was drawn
+    pygame.display.flip()
+    # Limit the game to 60 FPS
+    clock.tick(60)
+
+pygame.quit()
