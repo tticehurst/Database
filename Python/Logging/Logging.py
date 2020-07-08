00040d56501a7d2b4425092a54d0eb2f3c48abaf
O5X <tolly67p@gmail.com>
2020-07-08 17:06:20 +0100

    Paused on tutorial to try something

--- Python/Logging/Logging.py
+++ Python/Logging/Logging.py
@@ -1,10 +1,14 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG) # Tells it at what level to
+# logging.basicConfig(level=logging.DEBUG) # Tells it at what level to
 # start displaying the messages
+                                        # w = write mode                            # %(name)s - (displays 'root')
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M')
 
 logging.debug('This is a debug message')
 logging.info('This is an info message')
 logging.warning('This is an warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
+name = 'bob'
+logging.error('%s raised an error', name)
