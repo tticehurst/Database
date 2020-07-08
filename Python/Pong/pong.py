ff90db64343b3d05e3524727878602ea5787795d
O5X <tolly67p@gmail.com>
2020-07-08 17:49:36 +0100

    Window now stays open

--- Python/Pong/pong.py
+++ Python/Pong/pong.py
@@ -9,3 +9,16 @@ WHITE = (255,255,255)
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong!")
+
+# This loop will carry on until the user exists the game
+carryOn = True
+
+# The clock will be used to control how fast the screen updates
+clock = pygame.time.Clock()
+
+# Main program loop
+while carryOn:
+    # Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            carryOn = False
