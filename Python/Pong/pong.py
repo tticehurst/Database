7591f46b047145768c7d7bb1e1da81ab8ec8145b
O5X <tolly67p@gmail.com>
2020-07-09 18:01:17 +0100

    Reformatted pong.py code Moved ball and paddle classes to pong.py

--- Python/Pong/pong.py
+++ Python/Pong/pong.py
@@ -1,12 +1,68 @@
 import pygame
 from paddle import Paddle
 from ball import Ball
+from random import randint
 
 pygame.init()
 
 # Colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+
+# Paddle class
+class Paddle(pygame.sprite.Sprite):
+    # This class repesents a paddle
+    def __init__(self,color,width,height):
+        super().__init__()
+
+        # Pass in the color of the paddle, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width,height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        # Draw a paddle
+        pygame.draw.rect(self.image, color, [0,0, width, height])
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+    
+    def moveUp(self, pixels):
+        self.rect.y -= pixels # If y is lower than the pixels
+        # Checks that you are not going too far (off the screen)
+        if self.rect.y < 0:
+            self.rect.y = 0
+    
+    def moveDown(self, pixels):
+        self.rect.y += pixels # If y is higher than the pixels
+        if self.rect.y > 400:
+            self.rect.y = 400
+
+# Ball class
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+    
+         # Pass in the color of the ball, its width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        # Draw the ball
+        pygame.draw.rect(self.image, color, [0,0, width, height])
+        self.velocity = [randint(4,8),randint(-8,8)]
+        
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+    
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+    
+    def bounce(self):
+        self.velocity[0] = -self.velocity[0]
+        self.velocity[1] = randint(-8,8)
 
 # Open a new window
 size = (700, 500)
