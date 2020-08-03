bf7e53e87d60b8cba642eb8c196f821d1bfd4eda
O5X <tolly67p@gmail.com>
2020-08-03 16:56:49 +0100

    Auto

--- Java/SimpleTable/src/SimpleTable/Main.java
+++
@@ -1,8 +0,0 @@
-package SimpleTable;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ Java/TestTable/src/app/TableData.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class TableData {
+
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+	int screenHeight = screenSize.height;
+	int screenWidth = screenSize.width;
+
+	JFrame frame;
+	JTable table;
+	JScrollPane scrollPane;
+
+	TableData() {
+		frame = new JFrame();
+		frame.setTitle("Table");
+		String[][] data = { { "test", "test", "test" } };
+		String[] columnNames = { "column1", "column2", "column3" };
+
+		table = new JTable(data, columnNames);
+
+		scrollPane = new JScrollPane(table);
+		frame.add(scrollPane);
+		frame.setSize(screenWidth, screenHeight);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new TableData();
+
+	}
+}
