d2ca06b7f93ded4cf1ae40201398773fe9fc98e8
O5X <tolly67p@gmail.com>
2020-08-03 21:17:55 +0100

    Auto

--- Java/TestTable/src/app/README.md
+++ Java/TestTable/src/app/README.md
@@ -4,3 +4,4 @@
  * How to edit Jtable? 
    * How to make an editable table
    		* How to make a button inside a jtable
+   			* How to make a button
--- Java/TestTable/src/app/TableData.java
+++ Java/TestTable/src/app/TableData.java
@@ -3,11 +3,11 @@ package app;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.*;
 
 public class TableData {
+
+	WIPMessage WIP = new WIPMessage();
 
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
@@ -21,7 +21,7 @@ public class TableData {
 	TableData() {
 		frame = new JFrame();
 		frame.setTitle("Table");
-		String[][] data = { { "test", "test", "test", "delete" } };
+		String[][] data = { { "test", "test", "test", "" } };
 		String[] columnNames = { "column1", "column2", "column3", "column4" };
 
 		table = new JTable(data, columnNames);
@@ -34,6 +34,7 @@ public class TableData {
 
 	public static void main(String[] args) {
 		new TableData();
+		JOptionPane.showMessageDialog(null, messageLabel);
 
 	}
 }
